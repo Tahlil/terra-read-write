fca58494a8aa828ae7cc9313696f838945a8abd2
Tahlil <bsse0803@iit.du.ac.bd>
2022-06-30 01:02:53 +0600

    Connect to terra node

---
+++ connect.js
@@ -0,0 +1,12 @@
+const{ LCDClient} = require('@terra-money/terra.js');
+
+require('dotenv').config();
+
+const main = async () => {
+    const terra = new LCDClient({
+        URL: process.env.TERRA_NODE_URL,
+        chainID: process.env.TERRA_CHAIN_ID
+    });
+    console.log("Successfully connected to Terra node");
+    return terra;
+}
