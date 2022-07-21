fed89ec886208415f59e3ef6aa8aaa8e0b543fec
Tahlil <bsse0803@iit.du.ac.bd>
2022-07-21 22:51:16 +0600

    Generate address from mnemonic keys

---
+++ create_account.js
@@ -0,0 +1,18 @@
+const{ LCDClient, MnemonicKey } = require('@terra-money/terra.js');
+
+require('dotenv').config();
+
+const main = async () => {
+    const terra = new LCDClient({
+        URL: process.env.TERRA_NODE_URL,
+        chainID: process.env.TERRA_CHAIN_ID
+    });
+    const mk = new MnemonicKey();
+    console.log(mk.accAddress);
+}
+
+main().then((resp) => {
+    console.log(resp);
+}).catch((err) => {
+    console.error(err);
+})
